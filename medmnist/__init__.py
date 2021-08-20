c1407f130d76acfa5a0d1735d1f174c76dbc7b98
duducheng <jekyll4168@icloud.com>
2021-08-20 13:01:44 +0200

    fixed installation requirements

--- medmnist/__init__.py
+++ medmnist/__init__.py
@@ -1,5 +1,9 @@
 from medmnist.info import __version__, HOMEPAGE, INFO
-from medmnist.dataset import (PathMNIST, ChestMNIST, DermaMNIST, OCTMNIST, PneumoniaMNIST, RetinaMNIST,
-                              BreastMNIST, BloodMNIST, TissueMNIST, OrganAMNIST, OrganCMNIST, OrganSMNIST,
-                              OrganMNIST3D, NoduleMNIST3D, AdrenalMNIST3D, FractureMNIST3D, VesselMNIST3D, SynapseMNIST3D)
-from medmnist.evaluator import Evaluator
+try:
+    from medmnist.dataset import (PathMNIST, ChestMNIST, DermaMNIST, OCTMNIST, PneumoniaMNIST, RetinaMNIST,
+                                  BreastMNIST, BloodMNIST, TissueMNIST, OrganAMNIST, OrganCMNIST, OrganSMNIST,
+                                  OrganMNIST3D, NoduleMNIST3D, AdrenalMNIST3D, FractureMNIST3D, VesselMNIST3D, SynapseMNIST3D)
+    from medmnist.evaluator import Evaluator
+except:
+    print("Please install the required packages first." +
+          "Use `pip install -r requirements.txt`")
